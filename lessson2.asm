bf18bb12428b4bcec02cdf919ca205b89441e265
tak-ka3 <hi.ta9.23.333@gmail.com>
2021-06-26 22:31:58 +0900

    finish kadai3-2

--- lessson2.asm
+++ lessson2.asm
@@ -1,2 +1,7 @@
 addi r1, r0, 1 // 000001_00000_00001_0000000000000001_
 addi r2, r1, 1 //000001_00001_00010_0000000000000001_
+addi r6, r0, 6
+label1:     addi r7, r4, r6
+label5:     addi r8, r4, r1
+bne r9, r10 label1
+beq r11, r12 label5
